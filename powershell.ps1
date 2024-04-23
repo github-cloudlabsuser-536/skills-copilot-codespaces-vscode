2e3e340b891c82c3ad4db9fdbe5b6b0f444c3864
github-cloudlabsuser-536 <167230506+github-cloudlabsuser-536@users.noreply.github.com>
2024-04-23 17:52:14 +0000

    Copilot third commit

---
+++ powershell.ps1
@@ -0,0 +1,18 @@
+c# Login to Azure
+Connect-AzAccount
+
+# Set variables
+$resourceGroupName = "myResourceGroup"
+$location = "West Europe"
+$storageAccountName = "mystorageaccount"
+
+# Create a resource group
+New-AzResourceGroup -Name $resourceGroupName -Location $location
+
+# Create a storage account
+New-AzStorageAccount -ResourceGroupName $resourceGroupName `
+  -Name $storageAccountName `
+  -Location $location `
+  -SkuName Standard_LRS `
+  -Kind StorageV2 `
+  -EnableHttpsTrafficOnly $true
